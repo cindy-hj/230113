e6b9f3d178cf6ef0645bdd3d914ab0150800d4d6
cindy-hj <hj0304hj@gmail.com>
2023-01-13 21:47:17 +0900

    2023 01 13 21:47
    jquery 실습

--- scroll.js
+++ scroll.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-    // 스크롤을 제어 할때
-    // html, body를 선택해서 스크롤의 위치값 조절
+    // 스크롤을 제어 할때 html, body를 선택해서 스크롤의 위치값 조절
     // =>scrollTop, scrollLeft
     // =>animate 속성위치
     // let a = 0;
@@ -45,14 +44,14 @@ $(document).ready(function(){
         marginLeft:-ww*0.9/2
     });
 
-    // nth-child 이용 해도 됨!
+    // 1. 각 메뉴를 클릭하면 각자 스크롤 이동 값이 지정
     // $(".menu:nth-child(1)").click(function(){
     //     $("html, body").animate({
     //         scrollTop:0
     //     });
     // });
 
-    // 인덱스 이용 해도 됨!
+    // 2. 클릭한 메뉴의 index값 활용
     // $(".menu").click(function(){
     //     let menuIndex=$(this).index();
     //     $("html, body").animate({
@@ -60,82 +59,77 @@ $(document).ready(function(){
     //     });
     // });
 
-    // $("#menu1").click(function(){
-    //     $("html, body").animate({
-    //         scrollTop:0
-    //     });
-    // });
-    // $("#menu2").click(function(){
-    //     $("html, body").animate({
-    //         scrollTop:wh
-    //     });
-    // });
-    // $("#menu3").click(function(){
-    //     $("html, body").animate({
-    //         scrollTop:wh*2
-    //     });
-    // });
-    // $("#menu4").click(function(){
-    //     $("html, body").animate({
-    //         scrollTop:wh*3
-    //     });
-    // });
-    // $("#menu5").click(function(){
-    //     $("html, body").animate({
-    //         scrollTop:wh*4
-    //     });
-    // });
-
-    // let  = 1;
-    // $("#quick").click(function(){
-    //     $("html, body").animate({
-    //         scrollTop:wh*a
-    //     });
-    //     a++;
-        // if(a>4) {
+    // 메뉴 버튼 눌러서 스크롤이 중간에 있을때도 퀵메뉴 눌러서 스크롤 아래로 이동 가능해야한다
+    // 전역 변수 활용!
+    let allClick = 0;
+    $(".menu").click(function(){
+        allClick=$(this).index();
+       
+        // $("html, body").animate({
+        //     scrollTop:wh*allClick
+        // });
+        // if(allClick >= 4) {
         //     $('img').attr({
         //         src:'img/up.png'
-        //     }); 
-        //     a = 0;
+        //     });
+        //     allClick = 0;  
         // } else {
         //     $('img').attr({
         //         src:'img/down.png'
         //     });
         // }
-    // });
+        
+        // this를 쓰면 메뉴를 클릭하고 다른 메뉴를 클릭할때 이전 메뉴를 선택할 수 없으니 배경색을 다시 되돌릴 수 없음
+        // $(this).addClass("on",300);
 
-    // 메뉴버튼 눌러도 스크롤 이동해야하고 퀵메뉴 눌러도 스크롤 이동해야함
-    // 전역 변수 활용!
-    let allClick = 0;
-    $(".menu").click(function(){
-        allClick=$(this).index();
+        // 이전에 어떤 메뉴를 선택했는지 모르니 메뉴 전체에서 .on을 지우고 클릭한 메뉴만 배경색 바꿔줌
+        // $(".menu").removeClass("on",200);
+        // $(".menu").eq(allClick).addClass("on",200);
+    
+        scrollMove();
+    });
+    $("#quick").click(function(){
+        allClick++;
+        
+        // $("html, body").animate({
+        //     scrollTop:wh*allClick
+        // });
+        // // allClilck은 전역변수이므로 메뉴를 누르면 index값을 가지고있고 그 후에 퀵메뉴를 누르면 index값에 +1을 하는것
+        // if(allClick >= 4) {
+        //     $('img').attr({
+        //         src:'img/up.png'
+        //     });
+        //     allClick = -1; 
+        // } else {
+        //     $('img').attr({
+        //         src:'img/down.png'
+        //     });
+        // }
+
+        scrollMove();
+    });
+
+    // .menu를 클릭했을 때의 이벤트 내용과
+    // #quick을 클릭했을 때의 이벤트 내용이 동일함
+    // 동일한 실행문을 하나의 그룹화 할 수 있는 것이 함수
+    // .menu클릭시, #quick클릭시 함수만 호출하면 됨
+    function scrollMove() {
         $("html, body").animate({
             scrollTop:wh*allClick
         });
         if(allClick >= 4) {
             $('img').attr({
                 src:'img/up.png'
-            }); 
+            });
+            allClick = -1; 
         } else {
             $('img').attr({
                 src:'img/down.png'
             });
         }
-    });
-    $("#quick").click(function(){
-        allClick++;
-        $("html, body").animate({
-            scrollTop:wh*allClick
-        });
-    });
-    if(allClick >= 4) {
-        $('img').attr({
-            src:'img/up.png'
-        }); 
-    } else {
-        $('img').attr({
-            src:'img/down.png'
-        });
+
+        // 여기에 넣어주면 퀵메뉴로 스크롤 움직일때도 어떤 메뉴 보고 있는지 색 변화로 알수있음
+        $(".menu").removeClass("on",200);
+        $(".menu").eq(allClick).addClass("on",200);  
     }
-    $(this).addClass("on",300)
 });
