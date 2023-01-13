3284f86653f6cadeb6bdebf581ba35a9d497c62c
cindy-hj <hj0304hj@gmail.com>
2023-01-13 17:53:06 +0900

    2023 01 13 17:52
    jquery 실습

--- scroll.js
+++ scroll.js
@@ -22,6 +22,8 @@ $(document).ready(function(){
     //     document.write(divIndex);
     // });
 
+    // js에서 window크기는 스크롤 포함, css에서 100%는 스크롤 제외.. 오차범위 생긴다!
+    // 부모는 window크기로 맞추고 자식은 css에서 100% 맞추면 해결..!
     let ww=$(window).width();
     let wh=$(window).height();
 
@@ -43,49 +45,97 @@ $(document).ready(function(){
         marginLeft:-ww*0.9/2
     });
 
-    $("#menu1").click(function(){
-        $("html, body").animate({
-            scrollTop:0
-        });
-    });
-    $("#menu2").click(function(){
-        $("html, body").animate({
-            scrollTop:wh
-        });
-    });
-    $("#menu3").click(function(){
-        $("html, body").animate({
-            scrollTop:wh*2
-        });
-    });
-    $("#menu4").click(function(){
-        $("html, body").animate({
-            scrollTop:wh*3
-        });
-    });
-    $("#menu5").click(function(){
-        $("html, body").animate({
-            scrollTop:wh*4
-        });
-    });
+    // nth-child 이용 해도 됨!
+    // $(".menu:nth-child(1)").click(function(){
+    //     $("html, body").animate({
+    //         scrollTop:0
+    //     });
+    // });
 
-    let a = 1;
-    $("#quick").click(function(){
+    // 인덱스 이용 해도 됨!
+    // $(".menu").click(function(){
+    //     let menuIndex=$(this).index();
+    //     $("html, body").animate({
+    //         scrollTop:wh*menuIndex
+    //     });
+    // });
+
+    // $("#menu1").click(function(){
+    //     $("html, body").animate({
+    //         scrollTop:0
+    //     });
+    // });
+    // $("#menu2").click(function(){
+    //     $("html, body").animate({
+    //         scrollTop:wh
+    //     });
+    // });
+    // $("#menu3").click(function(){
+    //     $("html, body").animate({
+    //         scrollTop:wh*2
+    //     });
+    // });
+    // $("#menu4").click(function(){
+    //     $("html, body").animate({
+    //         scrollTop:wh*3
+    //     });
+    // });
+    // $("#menu5").click(function(){
+    //     $("html, body").animate({
+    //         scrollTop:wh*4
+    //     });
+    // });
+
+    // let  = 1;
+    // $("#quick").click(function(){
+    //     $("html, body").animate({
+    //         scrollTop:wh*a
+    //     });
+    //     a++;
+        // if(a>4) {
+        //     $('img').attr({
+        //         src:'img/up.png'
+        //     }); 
+        //     a = 0;
+        // } else {
+        //     $('img').attr({
+        //         src:'img/down.png'
+        //     });
+        // }
+    // });
+
+    // 메뉴버튼 눌러도 스크롤 이동해야하고 퀵메뉴 눌러도 스크롤 이동해야함
+    // 전역 변수 활용!
+    let allClick = 0;
+    $(".menu").click(function(){
+        allClick=$(this).index();
         $("html, body").animate({
-            scrollTop:wh*a
+            scrollTop:wh*allClick
         });
-        a++;
-        if(a>4) {
+        if(allClick >= 4) {
             $('img').attr({
                 src:'img/up.png'
             }); 
-            a = 0;
         } else {
             $('img').attr({
                 src:'img/down.png'
             });
         }
     });
-
-    
+    $("#quick").click(function(){
+        allClick++;
+        $("html, body").animate({
+            scrollTop:wh*allClick
+        });
+    });
+    if(allClick >= 4) {
+        $('img').attr({
+            src:'img/up.png'
+        }); 
+    } else {
+        $('img').attr({
+            src:'img/down.png'
+        });
+    }
+    $(this).addClass("on",300)
 });
